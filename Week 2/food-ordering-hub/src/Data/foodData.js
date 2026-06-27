6b3760c2518f2c64f1abdcb68e71ad4ad5bea080
Shaurya Gupta <gshaurya2708@gmail.com>
2026-06-27 22:24:14 +0530

    Add files via upload

---
+++ Week 2/food-ordering-hub/src/Data/foodData.js
@@ -0,0 +1,39 @@
+const foodData = [
+    {
+        id: 1,
+        name: "Margherita Pizza",
+        price: 299,
+        image: "/Images/Pizza.jpg",
+    },
+    {
+        id: 2,
+        name: "Cheese Bruger",
+        price: 199,
+        image: "/Images/Burger.jpg",
+    },
+    {
+        id: 3,
+        name: "French Fries",
+        price: 149,
+        image: "/Images/French_Fries.jpg",
+    },
+    {
+        id: 4,
+        name: "Pasta",
+        price: 249,
+        image: "/Images/Pasta.jpg",
+    },
+    {
+        id: 5,
+        name: "Sandwich",
+        price: 29,
+        image: "/Images/Sandwich.jpg",
+    },
+    {
+        id: 6,
+        name: "Chocolate Donut",
+        price: 189,
+        image: "/Images/Chocolate_Donut.jpg",
+    },
+];
+export default foodData;
