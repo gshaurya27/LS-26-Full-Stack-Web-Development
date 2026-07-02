3c55bab4e3a06f9f9738f800e4c1850bfe5394c2
Shaurya Gupta <gshaurya2708@gmail.com>
2026-07-02 17:38:57 +0530

    Delete Week3/25b0337-django-unchained/App_folder directory

--- Week3/25b0337-django-unchained/App_folder/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BountiesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bounties'
--- Week3/25b0337-django-unchained/App_folder/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Bounty
-
-class BountySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Bounty
-        fields = '__all__'
-        read_only_fields = ['owner', 'created_at']
